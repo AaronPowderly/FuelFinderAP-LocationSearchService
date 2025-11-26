0685a75857fab85fca5d1d61baa979065ac50cc0
AARONPOWDERLY-STUDEN <G00434425@atu.ie>
2025-11-26 15:30:25 +0000

    Added LocationSearchController

--- FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/Service/UserService.java
+++ FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/Service/UserService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService
+public class  UserService
 {
     private final List<UserInformation> store = new ArrayList<>();
 
